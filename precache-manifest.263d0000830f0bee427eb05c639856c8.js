f42fa9bf54b969b6b0106d901538c257f58bb42a
Ahmed Kamal <en.ahmed.mk@gmail.com>
2020-07-06 20:54:34 +0200

    Updates

---
+++ precache-manifest.263d0000830f0bee427eb05c639856c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c93543bb082b35b46f27d0eba7d1372",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb597365969356c03dab",
+    "url": "./static/css/main.be9a2696.chunk.css"
+  },
+  {
+    "revision": "8cb579200d96c86ef029",
+    "url": "./static/js/2.3857f76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3857f76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb597365969356c03dab",
+    "url": "./static/js/main.b0926919.chunk.js"
+  },
+  {
+    "revision": "d24c9871ae18b40c55d6",
+    "url": "./static/js/runtime-main.59731fd4.js"
+  }
+]);
